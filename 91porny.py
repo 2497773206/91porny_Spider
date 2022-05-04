49d6d1037410055468edbf0409835b13ba54caeb
2497773206 <26455360+2497773206@users.noreply.github.com>
2022-05-04 13:48:17 +0800

    Update 91porny.py
    增加了更新模式
    将需要更新的用户url添加到‘userurl.txt’
    line 67  更改需要更新的视频数量

--- 91porny.py
+++ 91porny.py
@@ -18,8 +18,8 @@ class WebDriver:
         return self.session.get(url, timeout=11, headers=self.head, verify=False)
     
 
-def user(url):
-    userName = getpage(url)[0]
+def get_user_scr(url):
+    userName = get_page_src(url)[0]
     userPath = '/data/Videos/91porny/' + userName
     if os.path.exists(userPath):
         pass
@@ -30,6 +30,7 @@ def user(url):
     bs = BeautifulSoup(resp.text, 'lxml')
     colVideoList = bs.find_all(attrs={'class':'text-truncate title text-sub-title mt-2'})
     for list in colVideoList:
+        time.sleep(random.randint(1, 3))
         title = list.text
         view_url = 'https://0ksy5j.jiuse710.com' + list.attrs['href']
         view = web.get(view_url)
@@ -44,15 +45,51 @@ def user(url):
             videoName = userPath + '/' + title + '.mp4'
             path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
             if os.path.exists(videoName):
-                print('目录下已存在相同文件')
+                print('目录下已存在相同文件:' + userName + '/' +  title)
                 pass
             else:
                 download_m3u8(path)
+                #print(userPath)
+                with open(os.getcwd() + "/download_info.txt","a",encoding='utf-8') as f:
+                    f.write(userPath + '/' + title + '.mp4' + '\n')
+
+def get_updata_scr(url):
+    userName = get_page_src(url)[0]
+    userPath = '/data/Videos/91porny/' + userName
+    if os.path.exists(userPath):
+        pass
+    else:
+        os.mkdir(userPath)
+    web = WebDriver(False)
+    resp = web.get(url)
+    bs = BeautifulSoup(resp.text, 'lxml')
+    colVideoList = bs.find_all(attrs={'class':'text-truncate title text-sub-title mt-2'})
+    videoSrc = colVideoList[:10]#需要下载视频的数量
+    for list in videoSrc:
+        time.sleep(random.randint(1, 3))
+        title = list.text
+        view_url = 'https://0ksy5j.jiuse710.com' + list.attrs['href']
+        view = web.get(view_url)
+        view_bs = BeautifulSoup(view.text, 'lxml')
+        #print(view_bs.title.string)
+        if view_bs.title.string == '视频因版权原因已被删除':
+            print('视频因版权原因已被删除')
+            pass
+        else:
+            view_video = view_bs.select_one("#videoShowPage video")
+            data_src = view_video.attrs["data-src"]
+            videoName = userPath + '/' + title + '.mp4'
+            path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
+            if os.path.exists(videoName):
+                print('目录下已存在相同文件:' + userName + '/' +  title)
+                pass
+            else:
+                download_m3u8(path)
+                #print(userPath)
                 with open(os.getcwd() + "/download_info.txt","a",encoding='utf-8') as f:
                     f.write(userPath + '/' + title + '.mp4' + '\n')
 
 def download_m3u8(path):
-    print(path)
     os.system(path)
 
 def user_url():
@@ -63,17 +100,28 @@ def user_url():
         if not mystr:
             break
         url = mystr
-        enter(url)
+        get_url(url)
 
-def enter(url):
-    end = int(getpage(url)[1]) + 1
+def updata_url():
+    path = r"updata.txt"#更新链接
+    file = open(path,"r",encoding="utf-8",errors="ignore")
+    while True:
+        mystr = file.readline()#表示一次读取一行
+        if not mystr:
+            break
+        url = mystr
+        time.sleep(random.randint(1, 3))
+        get_updata_scr(url)
+
+def get_url(url):
+    end = int(get_page_src(url)[1]) + 1
     for page in range(1, end):
         url_page = url + '?page=' + str(page)
         print('当前正在爬取：' + url_page)
         time.sleep(random.randint(1, 3))
-        user(url_page)
+        get_user_scr(url_page)
 
-def getpage(url):
+def get_page_src(url):
     web1 = WebDriver(False)
     resp1 = web1.get(url)
     bs1 = BeautifulSoup(resp1.text, 'lxml')
@@ -87,15 +135,19 @@ def getpage(url):
         return user_name, page_page
 
 if __name__ == '__main__':
-    print('1、文件爬取   2、手动爬取')
+    print('1、文件爬取   2、手动爬取   3、更新第一页')
     zone = input('请输入爬取模式：')
     if int(zone) == 1:
         file = open("download_info.txt", 'w').close()
-        file1 = open("userurl.txt", 'w').close()
         user_url()
         print('爬取完毕')
+        file1 = open("userurl.txt", 'w').close()
     elif int(zone) == 2:
         file = open("download_info.txt", 'w').close()
         url = input('请输入需要爬取的链接：')
-        enter(url)
+        get_url(url)
+        print('爬取完毕')
+    elif int(zone) == 3:
+        file = open("download_info.txt", 'w').close()
+        updata_url()
         print('爬取完毕')
