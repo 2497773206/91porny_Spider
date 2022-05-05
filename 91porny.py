7a9aac76fb986a8cddb329048011426d6395fb59
2497773206 <26455360+2497773206@users.noreply.github.com>
2022-05-05 09:13:47 +0800

    Update 91porny.py
    更新了本地文件判断逻辑
    更新了对标题字符的优化

--- 91porny.py
+++ 91porny.py
@@ -3,19 +3,24 @@ import os, time, re, random, requests, urllib3
 from bs4 import BeautifulSoup
 
 URL = 'https://0ksy5j.jiuse710.com'#免翻地址更改位置
-
+cookie = 'PHPSESSID=11054ff48d7f42812082f048303950e9;'
 
 class WebDriver:
     def __init__(self, is_proxy):
         self.session = requests.session()
         self.isProxy = is_proxy
         self.head = {
-            'user-agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'
+            'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
+            'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding':'gzip, deflate, br',
+            'accept-language':'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'sec-ch-ua-platform':'"Windows"',
+            'sec-fetch-dest':'document',
+            'cookie':cookie
         }
         requests.packages.urllib3.disable_warnings()
     def get(self, url):
-        return self.session.get(url, timeout=11, headers=self.head, verify=False)
+        return self.session.get(url, timeout=15, headers=self.head, verify=False)
     
 
 def get_user_scr(url):
@@ -30,24 +35,23 @@ def get_user_scr(url):
     bs = BeautifulSoup(resp.text, 'lxml')
     colVideoList = bs.find_all(attrs={'class':'text-truncate title text-sub-title mt-2'})
     for list in colVideoList:
-        time.sleep(random.randint(1, 3))
-        title = list.text
+        title = list.text.replace(':','').replace('(','[').replace(')',']').replace('（','[').replace('）',']').replace('“','[').replace('”',']').replace('/','').replace('"','').replace(' ','')
         view_url = 'https://0ksy5j.jiuse710.com' + list.attrs['href']
-        view = web.get(view_url)
-        view_bs = BeautifulSoup(view.text, 'lxml')
-        #print(view_bs.title.string)
-        if view_bs.title.string == '视频因版权原因已被删除':
-            print('视频因版权原因已被删除')
+        videoName = userPath + '/' + title + '.mp4'
+        if os.path.exists(videoName):
+            print('目录下已存在相同文件:' + userName + '/' +  title)
             pass
         else:
-            view_video = view_bs.select_one("#videoShowPage video")
-            data_src = view_video.attrs["data-src"]
-            videoName = userPath + '/' + title + '.mp4'
-            path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
-            if os.path.exists(videoName):
-                print('目录下已存在相同文件:' + userName + '/' +  title)
+            time.sleep(random.randint(1, 3))
+            view = web.get(view_url)
+            view_bs = BeautifulSoup(view.text, 'lxml')
+            if view_bs.title.string == '视频因版权原因已被删除':
+                print('视频因版权原因已被删除')
                 pass
             else:
+                view_video = view_bs.select_one("#videoShowPage video")
+                data_src = view_video.attrs["data-src"]
+                path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
                 download_m3u8(path)
                 #print(userPath)
                 with open(os.getcwd() + "/download_info.txt","a",encoding='utf-8') as f:
@@ -66,24 +70,23 @@ def get_updata_scr(url):
     colVideoList = bs.find_all(attrs={'class':'text-truncate title text-sub-title mt-2'})
     videoSrc = colVideoList[:10]#需要下载视频的数量
     for list in videoSrc:
-        time.sleep(random.randint(1, 3))
-        title = list.text
+        title = list.text.replace(':','').replace('(','[').replace(')',']').replace('（','[').replace('）',']').replace('“','[').replace('”',']').replace('/','').replace('"','').replace(' ','')
         view_url = 'https://0ksy5j.jiuse710.com' + list.attrs['href']
-        view = web.get(view_url)
-        view_bs = BeautifulSoup(view.text, 'lxml')
-        #print(view_bs.title.string)
-        if view_bs.title.string == '视频因版权原因已被删除':
-            print('视频因版权原因已被删除')
+        videoName = userPath + '/' + title + '.mp4'
+        if os.path.exists(videoName):
+            print('目录下已存在相同文件:' + userName + '/' +  title)
             pass
         else:
-            view_video = view_bs.select_one("#videoShowPage video")
-            data_src = view_video.attrs["data-src"]
-            videoName = userPath + '/' + title + '.mp4'
-            path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
-            if os.path.exists(videoName):
-                print('目录下已存在相同文件:' + userName + '/' +  title)
+            time.sleep(random.randint(1, 3))
+            view = web.get(view_url)
+            view_bs = BeautifulSoup(view.text, 'lxml')
+            if view_bs.title.string == '视频因版权原因已被删除':
+                print('视频因版权原因已被删除')
                 pass
             else:
+                view_video = view_bs.select_one("#videoShowPage video")
+                data_src = view_video.attrs["data-src"]
+                path = os.getcwd() + '/Settings/./ffmpeg -y -i ' + data_src.replace('&','\'&\'') + ' -vcodec copy -acodec copy -absf aac_adtstoasc ' + videoName
                 download_m3u8(path)
                 #print(userPath)
                 with open(os.getcwd() + "/download_info.txt","a",encoding='utf-8') as f:
@@ -110,7 +113,6 @@ def updata_url():
         if not mystr:
             break
         url = mystr
-        time.sleep(random.randint(1, 3))
         get_updata_scr(url)
 
 def get_url(url):
@@ -151,3 +153,4 @@ if __name__ == '__main__':
         file = open("download_info.txt", 'w').close()
         updata_url()
         print('爬取完毕')
+
